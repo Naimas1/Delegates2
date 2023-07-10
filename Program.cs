27df69b7590319c258669e23155d2aa4424bff0e
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-07-10 11:57:32 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,51 @@
-﻿namespace Delegates2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates2
 {
-    internal class Program
+    struct Fraction
     {
-        static void Main(string[] args)
+        public int Numerator { get; set; }
+        public int Denominator { get; set; }
+    }
+
+    static class FractionExtensions
+    {
+        public static Fraction MinFraction(this Fraction[] fractions)
         {
-            Console.WriteLine("Hello, World!");
+            return fractions.OrderBy(f => (double)f.Numerator / f.Denominator).First();
+        }
+
+        public static Fraction MaxFraction(this Fraction[] fractions)
+        {
+            return fractions.OrderByDescending(f => (double)f.Numerator / f.Denominator).First();
+        }
+    }
+
+    class Programfs
+    {
+        static void Main()
+        {
+            Fraction[] fractions = {
+            new Fraction { Numerator = 1, Denominator = 2 },
+            new Fraction { Numerator = 3, Denominator = 4 },
+            new Fraction { Numerator = 1, Denominator = 3 },
+            new Fraction { Numerator = 2, Denominator = 5 }
+        };
+
+            Fraction minFraction = fractions.MinFraction();
+            Fraction maxFraction = fractions.MaxFraction();
+
+            Console.WriteLine("Мінімальний дріб:");
+            Console.WriteLine($"{minFraction.Numerator}/{minFraction.Denominator}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Максимальний дріб:");
+            Console.WriteLine($"{maxFraction.Numerator}/{maxFraction.Denominator}");
         }
     }
 }
