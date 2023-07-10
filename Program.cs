2e71794fcce6a47b11a6294a0e364d6aa092bd79
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-07-10 11:44:53 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,48 @@
-﻿namespace Delegates2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates2
 {
-    internal class Program
+    static class StringExtensions
     {
-        static void Main(string[] args)
+        public static int CountSentences(this string str)
         {
-            Console.WriteLine("Hello, World!");
+            int count = 0;
+
+            foreach (char c in str)
+            {
+                if (IsSentenceSeparator(c))
+                    count++;
+            }
+
+            return count;
+        }
+
+        private static bool IsSentenceSeparator(char c)
+        {
+            return c == '.' || c == '?' || c == '!';
+        }
+    }
+
+    class Programaf
+    {
+        static void Main()
+        {
+            string[] strings = {
+            "Hello. How are you?",
+            "I'm doing well. Thank you!",
+            "This is a test sentence.",
+            "No punctuation marks here"
+        };
+
+            foreach (string str in strings)
+            {
+                int sentenceCount = str.CountSentences();
+                Console.WriteLine($"Рядок '{str}' містить {sentenceCount} пропозицій.");
+            }
         }
     }
 }
