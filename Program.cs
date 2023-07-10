84d1ee42e910767bdcae7892152b2473841024ea
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-07-10 10:24:08 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,36 @@
-﻿namespace Delegates2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates2
 {
-    internal class Program
+    static class StringExtensions
     {
-        static void Main(string[] args)
+        public static int LengthOfLastWord(this string text)
         {
-            Console.WriteLine("Hello, World!");
+            string[] words = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (words.Length > 0)
+            {
+                return words[words.Length - 1].Length;
+            }
+            return 0;
+        }
+    }
+
+    class Programi
+    {
+        static void Main()
+        {
+            string[] texts = { "Hello, world!", "Programming is fun", "  ", "OpenAI ChatGPT" };
+
+            foreach (string text in texts)
+            {
+                int length = text.LengthOfLastWord();
+                Console.WriteLine($"Рядок: '{text}', Довжина останнього слова: {length}");
+            }
         }
     }
 }
