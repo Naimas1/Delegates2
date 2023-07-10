498986262ce2c6fb1b5be8279a727957da4b0531
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-07-10 11:35:15 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,43 @@
-﻿namespace Delegates2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates2
 {
-    internal class Program
+    static class StringExtensions
     {
-        static void Main(string[] args)
+        public static int CountVowels(this string str)
         {
-            Console.WriteLine("Hello, World!");
+            int count = 0;
+
+            foreach (char c in str.ToLower())
+            {
+                if (IsVowel(c))
+                    count++;
+            }
+
+            return count;
+        }
+
+        private static bool IsVowel(char c)
+        {
+            return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+        }
+    }
+
+    class Programdf
+    {
+        static void Main()
+        {
+            string[] strings = { "Hello", "World", "OpenAI", "Language" };
+
+            foreach (string str in strings)
+            {
+                int vowelCount = str.CountVowels();
+                Console.WriteLine($"Рядок '{str}' містить {vowelCount} голосних.");
+            }
         }
     }
 }
