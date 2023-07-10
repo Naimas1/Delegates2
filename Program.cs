88941c83b5d13d3d7c774c16f0b882b612ea31db
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-07-10 11:51:45 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,63 @@
-﻿namespace Delegates2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates2
 {
-    internal class Program
+    class Person
     {
-        static void Main(string[] args)
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+
+    static class PersonExtensions
+    {
+        public static double AverageAge(this Person[] people)
         {
-            Console.WriteLine("Hello, World!");
+            int totalAge = 0;
+
+            foreach (var person in people)
+            {
+                totalAge += person.Age;
+            }
+
+            return (double)totalAge / people.Length;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Person[] people = {
+            new Person { FirstName = "Andrei", LastName = "Mixailovich", Age = 30 },
+            new Person { FirstName = "Marina", LastName = "Oleksandrivna", Age = 25 },
+            new Person { FirstName = "Dima", LastName = "Anatolievich", Age = 35 },
+            new Person { FirstName = "Bogdan", LastName = "Valerievich", Age = 40 }
+        };
+
+            Person youngestPerson = people.OrderBy(p => p.Age).First();
+            Person oldestPerson = people.OrderByDescending(p => p.Age).First();
+
+            Console.WriteLine("Молодша людина:");
+            Console.WriteLine($"Ім'я: {youngestPerson.FirstName}");
+            Console.WriteLine($"Прізвище: {youngestPerson.LastName}");
+            Console.WriteLine($"Вік: {youngestPerson.Age}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Старша людина:");
+            Console.WriteLine($"Ім'я: {oldestPerson.FirstName}");
+            Console.WriteLine($"Прізвище: {oldestPerson.LastName}");
+            Console.WriteLine($"Вік: {oldestPerson.Age}");
+
+            Console.WriteLine();
+
+            double averageAge = people.AverageAge();
+            Console.WriteLine($"Середній вік: {averageAge}");
         }
     }
 }
