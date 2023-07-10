4d5f0f850598d0caf896c3939d6cecc10d3f59c6
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-07-10 11:24:55 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,39 @@
-﻿namespace Delegates2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates2
 {
-    internal class Program
+    static class IntExtensions
     {
-        static void Main(string[] args)
+        public static bool IsPrime(this int number)
         {
-            Console.WriteLine("Hello, World!");
+            if (number <= 1)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+
+    class Programad
+    {
+        static void Main()
+        {
+            int[] numbers = { 10, 13, 20, 23, 30 };
+
+            foreach (int number in numbers)
+            {
+                bool isPrime = number.IsPrime();
+                Console.WriteLine($"Число {number} є простим: {isPrime}");
+            }
         }
     }
 }
