6f9bc003a4bd71ab8e7942581fa344d4a89e83ea
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-07-10 10:19:31 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,42 @@
-﻿namespace Delegates2
+using Delegates2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates2
 {
-    internal class Program
+    static class NumberExtensions
     {
-        static void Main(string[] args)
+        public static bool IsFibonacciNumber(this int number)
         {
-            Console.WriteLine("Hello, World!");
+
+            int a = 0;
+            int b = 1;
+
+            while (b < number)
+            {
+                int temp = b;
+                b = a + b;
+                a = temp;
+            }
+
+            return b == number;
+        }
+    }
+
+    class Programu
+    {
+        static void Main()
+        {
+            int[] numbers = { 0, 1, 2, 3, 5, 8, 13, 21, 34, 55 };
+
+            foreach (int number in numbers)
+            {
+                bool isFibonacci = number.IsFibonacciNumber();
+                Console.WriteLine($"{number} є числом Фібоначчі: {isFibonacci}");
+            }
         }
     }
 }
