fade52e6158d921732e68e186cfad24990b52edf
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-07-10 10:39:07 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Delegates2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates2
 {
-    internal class Program
+    static class ArrayExtensions
     {
-        static void Main(string[] args)
+        public static int[] Filter(this int[] array, Func<int, bool> predicate)
         {
-            Console.WriteLine("Hello, World!");
+            int[] filteredArray = Array.FindAll(array, predicate);
+            return filteredArray;
+        }
+    }
+
+    class Programp
+    {
+        static void Main()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            int[] filteredNumbers = numbers.Filter(num => num % 2 == 0);
+
+            Console.WriteLine("Парні числа:");
+            foreach (int num in filteredNumbers)
+            {
+                Console.WriteLine(num);
+            }
         }
     }
 }
