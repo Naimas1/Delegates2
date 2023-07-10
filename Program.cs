f3e253c6e2f59c403d23d63fa2cdeda1a51d4c80
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-07-10 10:29:44 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,54 @@
-﻿namespace Delegates2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates2
 {
-    internal class Program
+    public static class StringExtensions
     {
-        static void Main(string[] args)
+        public static bool AreBracketsValid(this string text)
         {
-            Console.WriteLine("Hello, World!");
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in text)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stack.Push(c);
+                }
+                else if (c == ')' || c == '}' || c == ']')
+                {
+                    if (stack.Count == 0)
+                        return false;
+
+                    char top = stack.Pop();
+
+                    if ((c == ')' && top != '(') ||
+                        (c == '}' && top != '{') ||
+                        (c == ']' && top != '['))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+
+    class Programo
+    {
+        static void Main()
+        {
+            string[] texts = { "{}[]", "(())", "[{}]", "[}", "[[{]}]" };
+
+            foreach (string text in texts)
+            {
+                bool isValid = text.AreBracketsValid();
+                Console.WriteLine($"Рядок: '{text}', Валідність дужок: {isValid}");
+            }
         }
     }
 }
